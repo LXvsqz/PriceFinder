4bd13c305c97817db3be1c5e2fb4361f8d24bb7f
Alex <avasquez31@miners.utep.edu>
2019-04-18 11:26:15 -0600

    Cosmetic changes to icons. Matching colors. LAST TODO: Add Functions when each button is pressed

--- src/Main.java
+++ src/Main.java
@@ -114,6 +114,7 @@ public class Main extends JFrame{
     //***********************************************************//
 
 
+
     private void addItem(String name, String url){
         Item newItem= new Item(name,url);
         itemList.addElement(newItem);
@@ -340,13 +341,13 @@ public class Main extends JFrame{
 
         JToolBar toolBar = new JToolBar("Price watch");
         //Buttons
-        JButton b1 = new JButton(createImageIcon("check.png"));
+        JButton b1 = new JButton(createImageIcon("blueCheck.png"));
         //b1.addActionListener(this::refreshButtonClicked);
         b1.setToolTipText("Check the price");
         b1.setFocusPainted(false);
         toolBar.add(b1);
 
-        JButton b2 = new JButton(createImageIcon("add.jpg"));
+        JButton b2 = new JButton(createImageIcon("addBlue.png"));
         //b2.addActionListener(this::AddButtonClicked);
         b2.setToolTipText("Add to the price finder");
         b2.setFocusPainted(false);
@@ -354,23 +355,60 @@ public class Main extends JFrame{
 
         JButton b3 = new JButton(createImageIcon("search.png"));
         //b3.addActionListener(this::AddButtonClicked);
+
         b3.setToolTipText("Search");
         b3.setFocusPainted(false);
         toolBar.add(b3);
 
-        JButton b4 = new JButton(createImageIcon("First.png"));
+        JButton b4 = new JButton(createImageIcon("blueLast.png"));
         //b4.addActionListener(this::AddButtonClicked);
         b4.setToolTipText("First item");
         b4.setFocusPainted(false);
         toolBar.add(b4);
 
-        JButton b5 = new JButton(createImageIcon("last.png"));
+        JButton b5 = new JButton(createImageIcon("blueFirst.png"));
         //b5.addActionListener(this::AddButtonClicked);
         b5.setToolTipText("Last item");
         b5.setFocusPainted(false);
         toolBar.add(b5);
 
         toolBar.addSeparator();
+
+        JButton b6= new JButton(createImageIcon("CheckGreen.png"));
+        b6.addActionListener(this::AddButtonClicked);
+        b6.setToolTipText("Item Price of Selected Item");
+        b6.setFocusPainted(false);
+        toolBar.add(b6);
+
+        JButton b7= new JButton(createImageIcon("launch.png"));
+        b7.addActionListener(this::AddButtonClicked);
+        b7.setToolTipText("Launch Item Web-page");
+        b7.setFocusPainted(false);
+        toolBar.add(b7);
+
+        JButton b8= new JButton(createImageIcon("EditGreen.png"));
+        b8.addActionListener(this::AddButtonClicked);
+        b8.setToolTipText("Edit Selected Item");
+        b8.setFocusPainted(false);
+        toolBar.add(b8);
+
+        JButton b9= new JButton(createImageIcon("minus.png"));
+        b9.addActionListener(this::AddButtonClicked);
+        b9.setToolTipText("Delete Selected Item");
+        b9.setFocusPainted(false);
+        toolBar.add(b9);
+
+        toolBar.addSeparator();
+
+        JButton bA= new JButton(createImageIcon("Questionblue.png"));
+        bA.addActionListener(this::AddButtonClicked);
+        bA.setToolTipText("App Information");
+        bA.setFocusPainted(false);
+        toolBar.add(bA);
+
+
+
+
 
 
         control.add(toolBar, BorderLayout.NORTH);
