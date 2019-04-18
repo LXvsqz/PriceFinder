fba0eed421759d4ec1c0b3864b9aabd7ad73ff99
Alex <avasquez31@miners.utep.edu>
2019-04-17 22:33:16 -0600

    Got the right thing to display in the list. Need to mess with colors changing when an item is clicked, then done. Also need to fix frame

---
+++ src/drawItem.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+
+import java.awt.Component;
+import javax.swing.ImageIcon;
+import javax.swing.JList;
+import javax.swing.JLabel;
+import javax.swing.ListCellRenderer;
+
+
+public class drawItem extends ItemView implements ListCellRenderer<Item>{
+
+
+    public drawItem(){
+        setOpaque(true);
+    }
+
+
+    @Override
+    public Component getListCellRendererComponent(JList<? extends Item> list, Item value, int index, boolean isSelected, boolean cellHasFocus) {
+
+
+        
+        setItem(value);
+        repaint();
+
+        return this;
+
+    }
+}
